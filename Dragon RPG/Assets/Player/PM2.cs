81b6047adf241488ad4ecca1f7e3b7db21574f57
Matt Cronan <mcronan81@gmail.com>
2018-03-08 13:37:33 -0600

    the one with the camera script
    Added a different camera script + new player movement.  Still needs tweaking.  Going to change folder structure around.

---
+++ Dragon RPG/Assets/Player/PM2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class PM2 : MonoBehaviour
+    {
+        private ThirdPersonCharacter character; // A reference to the ThirdPersonCharacter on the object
+        private Transform mainCamera;                  // A reference to the main camera in the scenes transform
+        private Vector3 cameraForwardDirection;             // The current forward direction of the camera
+        private Vector3 move;
+        private bool jumping;                      // the world-relative desired move direction, calculated from the camForward and user input.
+
+        
+        private void Start()
+        {
+            // get the transform of the main camera
+            if (Camera.main != null)
+            {
+                mainCamera = Camera.main.transform;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+            }
+
+            // get the third person character ( this should never be null due to require component )
+            character = GetComponent<ThirdPersonCharacter>();
+        }
+
+
+        private void Update()
+        {
+            if (!jumping)
+            {
+                jumping = Input.GetKey(KeyCode.Space);
+            }
+        }
+
+
+        // Fixed update is called in sync with physics
+        private void FixedUpdate()
+        {
+            // read inputs
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            bool crouch = Input.GetKey(KeyCode.C);
+
+            // calculate move direction to pass to character
+            if (mainCamera != null)
+            {
+                // calculate camera relative direction to move:
+                cameraForwardDirection = Vector3.Scale(mainCamera.forward, new Vector3(1, 0, 1)).normalized;
+                move = v*cameraForwardDirection + h*mainCamera.right;
+            }
+            else
+            {
+                // we use world-relative directions in the case of no main camera
+                move = v*Vector3.forward + h*Vector3.right;
+            }
+
+			// walk speed multiplier
+	        //if (Input.GetKey(KeyCode.LeftShift)) move *= .5f;
+
+
+            // pass all parameters to the character control script
+            character.Move(move, crouch, jumping);
+            jumping = false;
+        }
+    }
+}
+
