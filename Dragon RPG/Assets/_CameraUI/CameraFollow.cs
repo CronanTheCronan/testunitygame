74ba24b22d6c9e4b9de5ec12db4d1df96242c3aa
Matt Cronan <mcronan81@gmail.com>
2018-03-11 15:58:59 -0500

    changed folder structure

--- Dragon RPG/Assets/_CameraUI/CameraFollow.cs
+++ Dragon RPG/Assets/_CameraUI/CameraFollow.cs
@@ -2,27 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
-using UnityStandardAssets.Characters.FirstPerson;
 
 
 namespace RPG.CameraUI
 {
     public class CameraFollow : MonoBehaviour
     {
-
-        //    GameObject player;
-
-        //private Quaternion cameraRotation;
-        //private Quaternion characterTargetRotation;
-        //private Quaternion cameraTargetRotation;
-
-        //public float XSensitivity = 2f;
-        //public float YSensitivity = 2f;
-        //public bool clampVerticalRotation = true;
-        //public float MinimumX = -90F;
-        //public float MaximumX = 90F;
-        //public bool smooth;
-        //public float smoothTime = 5f;
 
         public Transform target;
         [SerializeField] float distance = 5.0f;
@@ -58,12 +43,6 @@ namespace RPG.CameraUI
             }
         }
 
-        //void Update()
-        //{
-
-        //}
-
-        // Update is called once per frame
         void LateUpdate()
         {
             if (target)
@@ -90,36 +69,6 @@ namespace RPG.CameraUI
             }
         }
 
-        //private void RotateView()
-        //{
-        //    LookRotation(player.transform, transform);
-        //}
-
-        //public void LookRotation(Transform character, Transform camera)
-        //{
-        //    float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
-        //    float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
-
-        //    characterTargetRotation *= Quaternion.Euler(0f, yRot, 0f);
-        //    cameraTargetRotation *= Quaternion.Euler(-xRot, 0f, 0f);
-
-        //    if (clampVerticalRotation)
-        //        cameraTargetRotation = ClampRotationAroundXAxis(cameraRotation);
-
-        //    if (smooth)
-        //    {
-        //        character.localRotation = Quaternion.Slerp(character.localRotation, characterTargetRotation,
-        //            smoothTime * Time.deltaTime);
-        //        camera.localRotation = Quaternion.Slerp(camera.localRotation, cameraTargetRotation,
-        //            smoothTime * Time.deltaTime);
-        //    }
-        //    else
-        //    {
-        //        character.localRotation = characterTargetRotation;
-        //        camera.localRotation = cameraTargetRotation;
-        //    }
-        //}
-
         public static float ClampAngle(float angle, float min, float max)
         {
             if (angle < -360F)
@@ -128,21 +77,5 @@ namespace RPG.CameraUI
                 angle -= 360F;
             return Mathf.Clamp(angle, min, max);
         }
-
-        //Quaternion ClampRotationAroundXAxis(Quaternion q)
-        //{
-        //    q.x /= q.w;
-        //    q.y /= q.w;
-        //    q.z /= q.w;
-        //    q.w = 1.0f;
-
-        //    float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-
-        //    angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
-
-        //    q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
-
-        //    return q;
-        //}
     }
 }
